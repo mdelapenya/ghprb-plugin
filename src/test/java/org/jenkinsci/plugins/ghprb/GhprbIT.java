d4d7e93d253ecc88b9a8b5003847af5f6348d4e6
Manuel de la Peña <manuel.delapenya@liferay.com>
2014-07-30 13:04:28 +0200

    [#160] Add a failing test removing old ones (REVERT)

--- src/test/java/org/jenkinsci/plugins/ghprb/GhprbIT.java
+++ src/test/java/org/jenkinsci/plugins/ghprb/GhprbIT.java
@@ -1,14 +1,22 @@
 package org.jenkinsci.plugins.ghprb;
 
-import antlr.ANTLRException;
-import com.coravy.hudson.plugins.github.GithubProjectProperty;
-import com.google.common.collect.Lists;
-import hudson.model.FreeStyleProject;
+import static com.google.common.collect.Lists.newArrayList;
+import static org.fest.assertions.Fail.fail;
+import static org.kohsuke.github.GHIssueState.OPEN;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.eq;
 import hudson.plugins.git.BranchSpec;
 import hudson.plugins.git.GitSCM;
 import hudson.plugins.git.UserRemoteConfig;
 import hudson.plugins.git.util.DefaultBuildChooser;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import net.sf.json.JSONObject;
+
 import org.joda.time.DateTime;
 import org.junit.Before;
 import org.junit.Rule;
@@ -16,7 +24,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.jvnet.hudson.test.JenkinsRule;
 import org.kohsuke.github.GHCommitPointer;
-import org.kohsuke.github.GHIssueComment;
 import org.kohsuke.github.GHPullRequest;
 import org.kohsuke.github.GHRateLimit;
 import org.kohsuke.github.GHRepository;
@@ -25,19 +32,7 @@ import org.kohsuke.github.GitHub;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
-
-import static com.google.common.collect.Lists.newArrayList;
-import static org.fest.assertions.Assertions.assertThat;
-import static org.kohsuke.github.GHIssueState.OPEN;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Matchers.anyString;
-import static org.mockito.Matchers.eq;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.spy;
+import antlr.ANTLRException;
 
 @RunWith(MockitoJUnitRunner.class)
 public class GhprbIT {
@@ -102,100 +97,8 @@ public class GhprbIT {
     }
 
     @Test
-    public void shouldBuildTriggersOnNewPR() throws Exception {
-        // GIVEN
-        FreeStyleProject project = jenkinsRule.createFreeStyleProject("PRJ");
-        GhprbTrigger trigger = new GhprbTrigger(
-                "user", "user", "", "*/1 * * * *", "retest this please", false, false, false, false, false, null
-        );
-        given(commitPointer.getSha()).willReturn("sha");
-        JSONObject jsonObject = provideConfiguration();
-        jenkinsRule.getPluginManager().getPlugin(GHPRB_PLUGIN_NAME).getPlugin().configure(null, jsonObject);
-        project.addProperty(new GithubProjectProperty("https://github.com/user/dropwizard"));
-         given(ghPullRequest.getNumber()).willReturn(1);
-
-        // Creating spy on ghprb, configuring repo
-        Ghprb ghprb = spy(trigger.createGhprb(project));
-        doReturn(ghprbGitHub).when(ghprb).getGitHub();
-        ghprb.getRepository().setHelper(ghprb);
-
-        // Configuring and adding Ghprb trigger
-        project.addTrigger(trigger);
-        project.getTriggers().keySet().iterator().next().configure(null, jsonObject);
-
-        // Configuring Git SCM
-        GitSCM scm = provideGitSCM();
-        project.setScm(scm);
-
-        trigger.start(project, true);
-        trigger.setHelper(ghprb);
-
-        // THEN
-        Thread.sleep(65000);
-        assertThat(project.getBuilds().size()).isEqualTo(1);
-    }
-
-    @Test
-    public void shouldBuildTriggersOnUpdatingNewCommitsPR() throws Exception {
-        // GIVEN
-        FreeStyleProject project = jenkinsRule.createFreeStyleProject("PRJ");
-        GhprbTrigger trigger = new GhprbTrigger(
-                "user", "user", "", "*/1 * * * *", "retest this please", false, false, false, false, false, null
-        );
-        given(commitPointer.getSha()).willReturn("sha").willReturn("sha").willReturn("newOne").willReturn("newOne");
-        given(ghPullRequest.getComments()).willReturn(Lists.<GHIssueComment>newArrayList());
-        JSONObject jsonObject = provideConfiguration();
-        jenkinsRule.getPluginManager().getPlugin(GHPRB_PLUGIN_NAME).getPlugin().configure(null, jsonObject);
-        project.addProperty(new GithubProjectProperty("https://github.com/user/dropwizard"));
-        given(ghPullRequest.getNumber()).willReturn(2).willReturn(2).willReturn(3).willReturn(3);
-        Ghprb ghprb = spy(trigger.createGhprb(project));
-        doReturn(ghprbGitHub).when(ghprb).getGitHub();
-        trigger.start(project, true);
-        trigger.setHelper(ghprb);
-        ghprb.getRepository().setHelper(ghprb);
-        project.addTrigger(trigger);
-        project.getTriggers().keySet().iterator().next().configure(null, jsonObject);
-        GitSCM scm = provideGitSCM();
-        project.setScm(scm);
-
-        // THEN
-        Thread.sleep(130000);
-        assertThat(project.getBuilds().size()).isEqualTo(2);
-    }
-
-    @Test
-    public void shouldBuildTriggersOnUpdatingRetestMessagePR() throws Exception {
-        // GIVEN
-        FreeStyleProject project = jenkinsRule.createFreeStyleProject("PRJ");
-        GhprbTrigger trigger = new GhprbTrigger(
-                "user", "user", "", "*/1 * * * *", "retest this please", false, false, false, false, false, null
-        );
-
-        given(commitPointer.getSha()).willReturn("sha");
-
-        GHIssueComment comment = mock(GHIssueComment.class);
-        given(comment.getBody()).willReturn("retest this please");
-        given(comment.getUpdatedAt()).willReturn(new DateTime().plusDays(1).toDate());
-        given(comment.getUser()).willReturn(ghUser);
-        given(ghPullRequest.getComments()).willReturn(newArrayList(comment));
-        given(ghPullRequest.getNumber()).willReturn(5).willReturn(5).willReturn(6).willReturn(6);
-        JSONObject jsonObject = provideConfiguration();
-        jenkinsRule.getPluginManager().getPlugin(GHPRB_PLUGIN_NAME).getPlugin().configure(null, jsonObject);
-        project.addProperty(new GithubProjectProperty("https://github.com/user/dropwizard"));
-
-        Ghprb ghprb = spy(trigger.createGhprb(project));
-        doReturn(ghprbGitHub).when(ghprb).getGitHub();
-        trigger.start(project, true);
-        trigger.setHelper(ghprb);
-        ghprb.getRepository().setHelper(ghprb);
-        project.addTrigger(trigger);
-        project.getTriggers().keySet().iterator().next().configure(null, jsonObject);
-        GitSCM scm = provideGitSCM();
-        project.setScm(scm);
-
-        // THEN
-        Thread.sleep(130000);
-        assertThat(project.getBuilds().size()).isEqualTo(2);
+    public void shouldFailOnNewPR() throws Exception {
+        fail("Failing for testing purpose");
     }
 
     // Utility
