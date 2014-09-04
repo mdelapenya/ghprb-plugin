20d7560939f46bd8d2e5ae27ab159bbc2d2c9642
Manuel de la Peña <manuel.delapenya@liferay.com>
2014-09-04 10:20:44 +0200

    WIP

--- src/test/java/org/jenkinsci/plugins/ghprb/GhprbIT.java
+++ src/test/java/org/jenkinsci/plugins/ghprb/GhprbIT.java
@@ -118,100 +118,18 @@ public class GhprbIT {
     }
 
     @Test
-    public void shouldBuildTriggersOnNewPR() throws Exception {
-        // GIVEN
-        FreeStyleProject project = jenkinsRule.createFreeStyleProject("PRJ");
-        GhprbTrigger trigger = new GhprbTrigger(
-                "user", "user", "", "*/1 * * * *", "retest this please", false, false, false, false, false, null, null, false
-        );
-        given(commitPointer.getSha()).willReturn("sha");
-        JSONObject jsonObject = provideConfiguration();
-        jenkinsRule.getPluginManager().getPlugin(GHPRB_PLUGIN_NAME).getPlugin().configure(null, jsonObject);
-        project.addProperty(new GithubProjectProperty("https://github.com/user/dropwizard"));
-         given(ghPullRequest.getNumber()).willReturn(1);
-
-        // Creating spy on ghprb, configuring repo
-        Ghprb ghprb = spy(trigger.createGhprb(project));
-        doReturn(ghprbGitHub).when(ghprb).getGitHub();
-        ghprb.getRepository().setHelper(ghprb);
-
-        // Configuring and adding Ghprb trigger
-        project.addTrigger(trigger);
-        project.getTriggers().keySet().iterator().next().configure(null, jsonObject);
-
-        // Configuring Git SCM
-        GitSCM scm = provideGitSCM();
-        project.setScm(scm);
-
-        trigger.start(project, true);
-        trigger.setHelper(ghprb);
-
-        // THEN
-        Thread.sleep(65000);
-        assertThat(project.getBuilds().toArray().length).isEqualTo(1);
+    public void shouldFailOnPurpose() throws Exception {
+        org.fest.assertions.Fail.fail("Failing on purpose");
     }
 
     @Test
-    public void shouldBuildTriggersOnUpdatingNewCommitsPR() throws Exception {
-        // GIVEN
-        FreeStyleProject project = jenkinsRule.createFreeStyleProject("PRJ");
-        GhprbTrigger trigger = new GhprbTrigger(
-                "user", "user", "", "*/1 * * * *", "retest this please", false, false, false, false, false, null, null, false
-        );
-        given(commitPointer.getSha()).willReturn("sha").willReturn("sha").willReturn("newOne").willReturn("newOne");
-        given(ghPullRequest.getComments()).willReturn(Lists.<GHIssueComment>newArrayList());
-        JSONObject jsonObject = provideConfiguration();
-        jenkinsRule.getPluginManager().getPlugin(GHPRB_PLUGIN_NAME).getPlugin().configure(null, jsonObject);
-        project.addProperty(new GithubProjectProperty("https://github.com/user/dropwizard"));
-        given(ghPullRequest.getNumber()).willReturn(2).willReturn(2).willReturn(3).willReturn(3);
-        Ghprb ghprb = spy(trigger.createGhprb(project));
-        doReturn(ghprbGitHub).when(ghprb).getGitHub();
-        trigger.start(project, true);
-        trigger.setHelper(ghprb);
-        ghprb.getRepository().setHelper(ghprb);
-        project.addTrigger(trigger);
-        project.getTriggers().keySet().iterator().next().configure(null, jsonObject);
-        GitSCM scm = provideGitSCM();
-        project.setScm(scm);
-
-        // THEN
-        Thread.sleep(130000);
-        assertThat(project.getBuilds().toArray().length).isEqualTo(2);
+    public void shouldFailOnPurpose2() throws Exception {
+        org.fest.assertions.Fail.fail("Failing on purpose");
     }
 
     @Test
-    public void shouldBuildTriggersOnUpdatingRetestMessagePR() throws Exception {
-        // GIVEN
-        FreeStyleProject project = jenkinsRule.createFreeStyleProject("PRJ");
-        GhprbTrigger trigger = new GhprbTrigger(
-                "user", "user", "", "*/1 * * * *", "retest this please", false, false, false, false, false, null, null, false
-        );
-
-        given(commitPointer.getSha()).willReturn("sha");
-
-        GHIssueComment comment = mock(GHIssueComment.class);
-        given(comment.getBody()).willReturn("retest this please");
-        given(comment.getUpdatedAt()).willReturn(new DateTime().plusDays(1).toDate());
-        given(comment.getUser()).willReturn(ghUser);
-        given(ghPullRequest.getComments()).willReturn(newArrayList(comment));
-        given(ghPullRequest.getNumber()).willReturn(5).willReturn(5).willReturn(6).willReturn(6);
-        JSONObject jsonObject = provideConfiguration();
-        jenkinsRule.getPluginManager().getPlugin(GHPRB_PLUGIN_NAME).getPlugin().configure(null, jsonObject);
-        project.addProperty(new GithubProjectProperty("https://github.com/user/dropwizard"));
-
-        Ghprb ghprb = spy(trigger.createGhprb(project));
-        doReturn(ghprbGitHub).when(ghprb).getGitHub();
-        trigger.start(project, true);
-        trigger.setHelper(ghprb);
-        ghprb.getRepository().setHelper(ghprb);
-        project.addTrigger(trigger);
-        project.getTriggers().keySet().iterator().next().configure(null, jsonObject);
-        GitSCM scm = provideGitSCM();
-        project.setScm(scm);
-
-        // THEN
-        Thread.sleep(130000);
-        assertThat(project.getBuilds().toArray().length).isEqualTo(2);
+    public void shouldFailOnPurpose3() throws Exception {
+        org.fest.assertions.Fail.fail("Failing on purpose");
     }
 
     // Utility
