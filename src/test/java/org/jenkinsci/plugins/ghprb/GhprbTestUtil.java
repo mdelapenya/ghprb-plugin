fec57705da3d7447084bc8d9ef98e59a186ee504
Manuel de la Peña <manuel.delapenya@liferay.com>
2014-10-15 09:04:42 +0200

    Remove class comment, adding author

--- src/test/java/org/jenkinsci/plugins/ghprb/GhprbTestUtil.java
+++ src/test/java/org/jenkinsci/plugins/ghprb/GhprbTestUtil.java
@@ -1,17 +1,3 @@
-/**
- * Copyright (c) 2000-2014 Liferay, Inc. All rights reserved.
- *
- * This library is free software; you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation; either version 2.1 of the License, or (at your option)
- * any later version.
- *
- * This library is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
- * details.
- */
-
 package org.jenkinsci.plugins.ghprb;
 
 import static com.google.common.collect.Lists.newArrayList;
@@ -32,6 +18,9 @@ import hudson.plugins.git.GitSCM;
 import hudson.plugins.git.UserRemoteConfig;
 import net.sf.json.JSONObject;
 
+/**
+ * @author mdelapenya (Manuel de la Peña)
+ */
 public class GhprbTestUtil {
 
 	public static final int INITIAL_RATE_LIMIT = 5000;
--- src/test/java/org/jenkinsci/plugins/ghprb/manager/GhprbBuildTimeFormatterTest.java
+++ src/test/java/org/jenkinsci/plugins/ghprb/manager/GhprbBuildTimeFormatterTest.java
@@ -1,17 +1,3 @@
-/**
- * Copyright (c) 2000-2014 Liferay, Inc. All rights reserved.
- *
- * This library is free software; you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation; either version 2.1 of the License, or (at your option)
- * any later version.
- *
- * This library is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
- * details.
- */
-
 package org.jenkinsci.plugins.ghprb.manager;
 
 import static org.fest.assertions.Assertions.assertThat;
@@ -20,6 +6,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
 
+/**
+ * @author mdelapenya (Manuel de la Peña)
+ */
 @RunWith(MockitoJUnitRunner.class)
 public class GhprbBuildTimeFormatterTest {
 
